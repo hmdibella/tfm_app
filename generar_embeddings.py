aee8ceab6ac18431daa0b7e340c974ef8083825d
hmdibella <74025751+hmdibella@users.noreply.github.com>
2024-09-01 20:48:27 +0200

    fixed errors + new parameters
    fixed errors and added new options to the ini file, to select the chunk size for the embedding model

--- generar_embeddings.py
+++ generar_embeddings.py
@@ -2,21 +2,28 @@ from PyPDF2 import PdfReader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import tqdm
 import time
 import configparser
 import getopt, sys
+import math
+import warnings
+
+
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 HUGGINGFACEHUB_API_TOKEN = ""
 GOOGLE_API_KEY = ""
-MODEL_EMBEDDINGS_OLLAMA = ""
-MODEL_EMBEDDINGS_GOOGLE = ""
-OUTPUT_PATH = ""
-FAISS_GOOGLE_PATH = ""
-FAISS_OLLAMA_PATH = ""
-PDFS_PATH = ""
-NUMEXPR_MAX_THREADS = ""
+MODEL_EMBEDDINGS_OLLAMA = "paraphrase-xlm-r-multilingual-v1"
+MODEL_EMBEDDINGS_GOOGLE = "models/embedding-001"
+OUTPUT_PATH = "output"
+FAISS_GOOGLE_PATH = "output/faiss_index_google"
+FAISS_OLLAMA_PATH = "output/faiss_index"
+PDFS_PATH = "PDFs"
+NUMEXPR_MAX_THREADS = "16"
+CHUNK_SIZE = 10000
 
 
 def load_config():
@@ -29,6 +36,8 @@ def load_config():
     global FAISS_OLLAMA_PATH
     global LOG_PATH
     global NUMEXPR_MAX_THREADS
+    global PDFS_PATH
+    global CHUNK_SIZE
     
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -41,6 +50,8 @@ def load_config():
     FAISS_OLLAMA_PATH = config['DEFAULT']['faiss_ollama_path']
     NUMEXPR_MAX_THREADS = config['DEFAULT']['numexpr_max_threads']
     LOG_PATH = config['DEFAULT']['log_path']
+    PDFS_PATH = config['DEFAULT']['pdfs_path']
+    CHUNK_SIZE = int(config['DEFAULT']['chunk_size'])
 
 
 def get_pdf_text(pdf_docs):
@@ -51,12 +62,19 @@ def get_pdf_text(pdf_docs):
     return text
     
     
-def generate_raw_file()
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = CHUNK_SIZE, chunk_overlap = math.ceil(CHUNK_SIZE*0.1))
+    chunks = text_splitter.split_text(text)
+    return chunks
+    
+    
+def generate_raw_file():
     for filename in os.listdir(PDFS_PATH + '/'):
-    if filename.endswith(".pdf"):
-        raw_text += get_pdf_text(PDFS_PATH + '/' + filename)
+        if filename.endswith(".pdf"):
+            raw_text += get_pdf_text(PDFS_PATH + '/' + filename)
     with open(PDFS_PATH + '/rawdata.txt', 'w', encoding='utf-8') as archivo:
-    archivo.write(raw_text)
+        archivo.write(raw_text)
+    return len(raw_text)
     
     
 def chunker_google(seq, size):
@@ -91,11 +109,7 @@ def get_vector_store_ollama(text_chunks):
     vector_store.save_local(FAISS_OLLAMA_PATH)
     
     
-    
 def main():
-    print("Starting...")
-    load_config()
-    
     # parametros de entrada por linea de comandos
     argumentList = sys.argv[1:]
     options = "hgm:"
@@ -109,7 +123,6 @@ def main():
         gen_raw_file = False
         mode = "0"  ## 0 is Ollama; 1 is Google
         for currentArgument, currentValue in arguments:
-
             if currentArgument in ("-g", "--gen_raw"):
                 gen_raw_file = True
             elif currentArgument in ("-m", "--mode") and currentValue in ("0", "1"):
@@ -122,33 +135,37 @@ Uso: generar_embeddings [opciones]
     -g, --gen_raw: generate el archivo rawdata.txt desde cero con los archivos PDFs en la carpeta. Default no lo genera.
     -m, --mode: 0 para usar el modo Ollama, 1 para el modo Google. Default es 0.
 """)
+                return
             
     except getopt.error as err:
         # output error, and return with an error code
         print (str(err))
     
-    print("Working in {0} mode.".format("Ollama" if mode = "0" else "Google"))
+    print("Comenzando...")
+    load_config()
+    
+    print("Trabajando en modo {0}.".format("Ollama" if mode == "0" else "Google"))
     
     if ( gen_raw_file ):
-        print("Generando rawdata file...")
-        generate_raw_file()
-        print("Generando rawdata file... OK")
+        print("Generando archivo rawdata...")
+        length = generate_raw_file()
+        print("Generando archivo rawdata OK. Tamaño: " + str(length) )
     else:
-        print("No se genera el rawdata file.")
+        print("No se genera el archivo rawdata.")
         
-    print("Generando los chunks...")
+    print("Generando los chunks. Chunk size=" + str(CHUNK_SIZE))
     text_chunks = ""
     with open(PDFS_PATH + '/rawdata.txt', 'r', encoding='utf-8') as archivo:
         raw_text1 = archivo.read()
         text_chunks = get_text_chunks(raw_text1)
-    print("Generando los chunks... OK")
+    print("Generando los chunks OK.")
     
-    print("Generando los FAISS...")
-    if (mode = "0"):
+    print("Generando los FAISS. Modelo=" + MODEL_EMBEDDINGS_OLLAMA)
+    if (mode == "0"):
         get_vector_store_ollama(text_chunks)
     else:
         get_vector_store_google(text_chunks)
-    print("Generando los FAISS... OK")
+    print("Generando los FAISS OK.")
     
     print("Terminado.")
     
