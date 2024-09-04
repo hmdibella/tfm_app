6ed68ef10feecb75165d96aa8e5a3ac0f60621d5
hmdibella <74025751+hmdibella@users.noreply.github.com>
2024-09-04 18:30:25 +0200

    agregue historial del chat

--- streamlit_prueba_history.py
+++ streamlit_prueba_history.py
@@ -1,17 +1,22 @@
 import streamlit as st
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-#import google.generativeai as genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
 from langchain_community.llms import Ollama
 from langchain_community.llms import HuggingFaceHub
-from langchain.chains import LLMChain
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
 import time
 import logging
+import random
+import string
 import configparser
 import warnings
 
@@ -27,6 +32,49 @@ OUTPUT_PATH = "output"
 FAISS_GOOGLE_PATH = "output/faiss_index_google"
 FAISS_OLLAMA_PATH = "output/faiss_index"
 NUMEXPR_MAX_THREADS = "16"
+
+CONTEXTUALIZE_Q_SYSTEM_PROMPT = """Given a chat history and the latest user question which might reference context in the chat history, formulate a standalone question which can be understood without the chat history. Do NOT answer the question, just reformulate it if needed and otherwise return it as is."""
+
+PROMPT_TEMPLATE_LLAMA = """You are an intelligent assistant. Answer the question based solely on the information provided in the context, specific for Spain.
+Do not add any information beyond what is given. 
+If the context does not contain the answer, respond with: "The answer is not available in the provided context."
+Be concise and accurate. All the answers must be in Spanish language from Spain, in an informal manner.
+Answers must be clear, precise, and unambiguous, and a teenager should be able to understand the answer.
+Explicitly avoid phrases such as "según el documento", "según el capítulo", "en el texto", "como se menciona en el artículo", or any implication of external texts. Do not construct questions that require knowledge of the structure of the document or the location of information in it.
+Include the content-specific information that supports the answer to allow the answer to be independent of any external text.
+If the content lacks sufficient information to form a complete answer, do not force one.
+Create the answers in your own words; Direct copying of content is not permitted.
+NEVER mention the words "documento", "texto", "presentación", "archivo", "tabla", "artículo", "ley", "capítulo", "preámbulo", "título preliminar", "disposición" or "disposiciones generales" in your questions or answers.
+ALWAYS make sure that all answers are accurate, self-contained, and relevant, without relying on any original document or text or implying its existence, strictly avoiding any invention or speculation.
+IMPORTANT: if in the question there is no mention of a Comunidad Autonoma or the name of a city or province, try that the answer applies to Spain as a country.
+
+Context:
+{context}
+
+"""
+
+PROMPT_TEMPLATE_GEMMA = """<start_of_turn>user
+You are an intelligent assistant. Answer the question based solely on the information provided in the context, specific for Spain. 
+Do not add any information beyond what is given. 
+If the context does not contain the answer, respond with: "The answer is not available in the provided context."
+Be concise and accurate. All the answers must be in Spanish language from Spain, in an informal manner.
+Answers must be clear, precise, and unambiguous.
+Give the answers like I was not a lawyer or an expert in the legal area.
+Explicitly avoid phrases such as "según el documento", "según el capítulo", "en el texto", "como se menciona en el artículo", or any implication of external texts. Do not construct questions that require knowledge of the structure of the document or the location of information in it.
+Include the content-specific information that supports the answer to allow the answer to be independent of any external text.
+If the content lacks sufficient information to form a complete answer, do not force one.
+Create the answers in your own words; Direct copying of content is not permitted.
+NEVER mention the words "documento", "texto", "presentación", "archivo", "tabla", "artículo", "ley", "capítulo", "preámbulo", "título preliminar", "disposición" or "disposiciones generales" in your questions or answers.
+ALWAYS make sure that all answers are accurate, self-contained, and relevant, without relying on any original document or text or implying its existence, strictly avoiding any invention or speculation.
+IMPORTANT: if in the question there is no mention of a Comunidad Autonoma or the name of the city/province, try that the answer applies to Spain as a country.
+
+Context:
+{context}
+
+<start_of_turn>model
+"""
+
+store = {}
 
 
 def load_config():
@@ -53,59 +101,51 @@ def load_config():
     LOG_PATH = config['DEFAULT']['log_path']
 
 
-def get_conversational_chain_ollama(model_name):
-    prompt_template_llama = """
-You are an intelligent assistant. Answer the question based solely on the information provided in the context, specific for Spain. Also consider the previous conversations you had with the user, to answer to the question.
-Do not add any information beyond what is given. 
-If the context does not contain the answer, respond with: "The answer is not available in the provided context."
-Be concise and accurate. All the answers must be in Spanish language from Spain, in an informal manner.
-Answers must be clear, precise, and unambiguous.
-Give the answers like the user is not a lawyer or an expert in the legal area.
-Explicitly avoid phrases such as "según el documento", "según el capítulo", "en el texto", "como se menciona en el artículo", or any implication of external texts. Do not construct questions that require knowledge of the structure of the document or the location of information in it.
-Include the content-specific information that supports the answer to allow the answer to be independent of any external text.
-If the content lacks sufficient information to form a complete answer, do not force one.
-Create the answers in your own words; Direct copying of content is not permitted.
-NEVER mention the words "documento", "texto", "presentación", "archivo", "tabla", "artículo", "ley", "capítulo", "preámbulo", "título preliminar", "disposición" or "disposiciones generales" in your questions or answers.
-ALWAYS make sure that all answers are accurate, self-contained, and relevant, without relying on any original document or text or implying its existence, strictly avoiding any invention or speculation.
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    global store
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
 
-Context:
-{context}
 
-Question:
-{question}
-
-Answer:
-"""
-    prompt_template_gemma = """<start_of_turn>user
-You are an intelligent assistant. Answer the question based solely on the information provided in the context, specific for Spain. 
-Do not add any information beyond what is given. 
-If the context does not contain the answer, respond with: "The answer is not available in the provided context."
-Be concise and accurate. All the answers must be in Spanish language from Spain, in an informal manner.
-Answers must be clear, precise, and unambiguous.
-Give the answers like I was not a lawyer or an expert in the legal area.
-Explicitly avoid phrases such as "según el documento", "según el capítulo", "en el texto", "como se menciona en el artículo", or any implication of external texts. Do not construct questions that require knowledge of the structure of the document or the location of information in it.
-Include the content-specific information that supports the answer to allow the answer to be independent of any external text.
-If the content lacks sufficient information to form a complete answer, do not force one.
-Create the answers in your own words; Direct copying of content is not permitted.
-NEVER mention the words "documento", "texto", "presentación", "archivo", "tabla", "artículo", "ley", "capítulo", "preámbulo", "título preliminar", "disposición" or "disposiciones generales" in your questions or answers.
-ALWAYS make sure that all answers are accurate, self-contained, and relevant, without relying on any original document or text or implying its existence, strictly avoiding any invention or speculation.
-IMPORTANT if in the question there is no mention of a Comunidad Autonoma or the name of the city/province, try that the answer applies to Spain as a country.
-
-Context:
-{context}
-
-Question:
-{question}
-
-<start_of_turn>model
-"""
-    # Usar Ollama como LLM
-    model = Ollama(model=model_name)
+def get_conversational_chain_ollama(model_name, retriever):
     
-    prompt_template = prompt_template_llama if model_name.startswith('llama') else prompt_template_gemma
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question", "prev_conv"])
-    chain = LLMChain(llm=model, prompt=prompt)
-    return chain
+    # Usar Ollama como LLM
+    llm = Ollama(model=model_name)
+    
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", CONTEXTUALIZE_Q_SYSTEM_PROMPT),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+    
+    prompt_template = PROMPT_TEMPLATE_LLAMA if model_name.startswith('llama') else PROMPT_TEMPLATE_GEMMA
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", prompt_template),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+    
+    return conversational_rag_chain
     
     
 def get_conversational_chain_google():
@@ -139,12 +179,18 @@ Answer:
     return chain
 
 
-def user_input_ollama(user_question, model_name1, prev_conv, chunk_size, k_value):
+def user_input_ollama(user_question, model_name1, chunk_size, k_value, session_id):
     embeddings = HuggingFaceEmbeddings(model_name=MODEL_EMBEDDINGS_OLLAMA)
     new_db = FAISS.load_local(FAISS_OLLAMA_PATH + "_" + chunk_size, embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_question, k=int(k_value))
-    chain = get_conversational_chain_ollama(model_name1)
-    response = chain.run({"context": docs, "question": user_question, "prev_conv": prev_conv})
+    retriever = new_db.as_retriever(search_kwargs={"k": k_value})
+    chain = get_conversational_chain_ollama(model_name1, retriever)
+    response = chain.invoke(
+        {"input": user_question},
+        config={
+            "configurable": {"session_id": session_id}
+        },
+    )["answer"]
+    
     return response
     
     
@@ -158,14 +204,14 @@ def user_input_google(user_question, prev_conv, chunk_size, k_value):
     return response["output_text"]
     
     
-def response_generator(prompt, model_name, prev_conv, chunk_size, k_value):
+def response_generator(prompt, model_name, chunk_size, k_value, session_id):
     start_model_exec = time.time()
     if model_name.startswith('Gemini'):
         response = user_input_google(prompt, prev_conv, chunk_size, k_value)
     else:
-        response = user_input_ollama(prompt, model_name, prev_conv, chunk_size, k_value)
+        response = user_input_ollama(prompt, model_name, chunk_size, k_value, session_id)
     end_model_exec = time.time()
-    resp_text = "{0} - {3} - {4}: {1}\n\n(Tiempo de respuesta: {2:.2f} seg.).".format(model_name, response, end_model_exec - start_model_exec, chunk_size, k_value)
+    resp_text = "{0} - {3} - {4} - {5}: {1}\n\n(Tiempo de respuesta: {2:.2f} seg.).".format(model_name, response, end_model_exec - start_model_exec, chunk_size, k_value, session_id)
     for word in resp_text.split():
         yield word + " "
         time.sleep(0.05)
@@ -177,12 +223,15 @@ def main():
     os.environ["NUMEXPR_MAX_THREADS"] = NUMEXPR_MAX_THREADS
     os.environ['HUGGINGFACEHUB_API_TOKEN'] = HUGGINGFACEHUB_API_TOKEN
     os.environ['GOOGLE_API_KEY'] = GOOGLE_API_KEY
-    #genai.configure(api_key = GOOGLE_API_KEY)
 
     logger = logging.getLogger(__name__)
     log_ruta = LOG_PATH + '/streamlit_prueba.log'
     logging.basicConfig(filename=log_ruta, level=logging.INFO, 
     format='%(asctime)s.%(msecs)03d %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    
+    # Initialization
+    if 'session_id' not in st.session_state:
+        st.session_state['session_id'] = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
   
     st.set_page_config(page_title="Chat legal v1.0", page_icon="🇪🇸")  # Set title and icon
   
@@ -204,9 +253,9 @@ def main():
         '5000', 
         '2000',
         '1000'],
-        index=0 )
+        index=1 )
         
-    k_value = st.sidebar.number_input( "Inserta el valor de k", value=1, placeholder="Ingresa un entero...", min_value=1, format="%d")
+    k_value = st.sidebar.number_input( "Inserta el valor de k", value=16, placeholder="Ingresa un entero...", min_value=1, format="%d")
     
     st.header("Chat con el modelo", divider="gray")
   
@@ -238,7 +287,7 @@ def main():
             # Display assistant response in chat message container
             with st.chat_message("assistant"):
                 #response = st.write_stream(response_generator(prompt, model_chat, st.session_state.context, chunk_size, k_value))
-                response = st.write_stream(response_generator(prompt, model_chat, "", chunk_size, k_value))
+                response = st.write_stream(response_generator(prompt, model_chat, chunk_size, k_value, st.session_state['session_id']))
 
             # Add assistant response to chat history
             st.session_state.messages.append({"role": "assistant", "content": response})
